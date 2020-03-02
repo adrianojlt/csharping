d24b52b16efe653a8cab8540b1779ff360f3cab1
Adriano Teixeira <adrianojlt@gmail.com>
2020-03-02 22:58:21 +0000

    Delegates Examples

---
+++ csharpTT/Tests/Delegates/DeletegatesTests.cs
@@ -0,0 +1,37 @@
+﻿namespace csharpTT.Tests.Delegates
+{
+    using csharpTT.Generics;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DeletegatesTests
+    {
+        private string[] names = { "Alice", "john", "Bobby", "Kyle", "Tod", "Armin", "George" };
+
+        [Test]
+        public void Delegates_Names_LessThanFive()
+        {
+            // Arrange
+            var delegates = new DelegatesExample();
+
+            // Assert
+            var filteredNames = delegates.NamesFilter(names, delegates.LessThanFive);
+
+            // Result
+            Assert.AreEqual(filteredNames.Count, 3);
+        }
+
+        [Test]
+        public void Delegates_Names_MoreThanFive()
+        {
+            // Arrange
+            var delegates = new DelegatesExample();
+
+            // Assert
+            var filteredNames = delegates.NamesFilter(names, name => name.Length > 5);
+
+            // Result
+            Assert.AreEqual(filteredNames.Count, 1);
+        }
+    }
+}
